cad5eef96213c3836d97d3c1c4282ed791c377f4
Dokkae <90514461+Dokkae6949@users.noreply.github.com>
2022-07-16 12:44:10 +0200

    Created dupe of dokkaes scene for testing

--- Assets/_Project/Scripts/Systems/Weapon-Ray/SOWeapon.cs
+++ Assets/_Project/Scripts/Systems/Weapon-Ray/SOWeapon.cs
@@ -7,16 +7,16 @@ namespace Game.WeaponSystem
     [CreateAssetMenu(fileName = "Weapon", menuName = "Weapons/Weapon")]
     public class SOWeapon : ScriptableObject
     {
-        public int damage;
+        [Range(0, 1000)] public int damage;
 
-        public int damageMultiplier = 1;
+        [Range(1, 200)] public int damageMultiplier = 1;
 
-        public int shotsPerSecond = 1;
+        [Range(0.01f, 200)] public int shotsPerSecond = 1;
 
-        public float bulletSpray = 0;
+        [Range(0.01f, 200)] public float bulletSpray = 0;
 
-        public float maxBulletRange = 100;
+        [Range(1, 500)] public float maxBulletRange = 100;
 
-        public int bulletsPerShot = 1;
+        [Range(1, 100)] public int bulletsPerShot = 1;
     }
 }
